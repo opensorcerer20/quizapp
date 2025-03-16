ea74404c0da926c5eebbb169634c45bec6b6d05f
opensorcerer20 <harryjry@yahoo.com>
2025-03-16 09:23:41 -0500

    reduced jest requirements

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 /** @type {import('jest').Config} */
-module.exports = {
+ module.exports = {
     preset: "jest-expo",
     moduleDirectories: ["node_modules", "./util/tests", __dirname],
     transformIgnorePatterns: [
-      "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@realm/.*|@sentry/.*|uuid|expo-asset/.*|expo-font/.*)",
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|expo-asset/.*|expo-font/.*)",
     ],
     transform: {
-      "\\.[jt]sx?$": "babel-jest",
+        "\\.[jt]sx?$": "babel-jest",
     },
-};
+ };
