bec4c4cc4ad3ce7b8bb61ffb4bb4db315c114eb9
opensorcerer20 <harryjry@yahoo.com>
2025-07-14 20:39:10 -0500

    fix android error, fixed list bottom clip

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,5 +1,4 @@
-import { StyleSheet } from "react-native";
-import { View } from "react-native-web";
+import { StyleSheet, View } from "react-native";
 
 import { getBgScheme } from "../common/util";
 
