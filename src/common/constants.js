c3b4ef1889f5d54a84924ee6f8b02efeddfdd42d
opensorcerer20 <opensorcerer20@gmail.com>
2025-12-19 01:07:30 -0600

    fixes for 0.7.0

--- src/common/constants.js
+++ src/common/constants.js
@@ -30,9 +30,6 @@ export const MIME_TYPE_CSV = ["text/csv", "text/comma-separated-values"];
 export const MIME_TYPE_TEXT = ["text/plain"];
 
 const { width } = Dimensions.get("window");
-export const SAFE_MARGIN = Math.round(width / 20); // 5% width
-// export const SAFE_WIDTH = width - Math.round(width / 20); // 95% width
-// @todo TESTING WIDTH
 export const SAFE_WIDTH = width; // 95% width
 
 // source: https://gist.github.com/aminnj/5ca372aa2def72fb017b531c894afdca
